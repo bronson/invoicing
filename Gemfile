00b62998b3089edc19225edb5674e700577f748c
Scott Bronson <brons_git@rinspin.com>
2014-09-29 00:15:45 -0700

    write html template in slim

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
+gem 'slim'
 gem 'pdfkit'
 gem 'wkhtmltopdf-heroku', git: 'https://github.com/Thinkei/wkhtmltopdf-heroku'
--- gen.rb
+++ gen.rb
@@ -3,6 +3,8 @@
 require 'bundler/setup'
 require 'wkhtmltopdf/heroku'
 require 'pdfkit'
+require 'slim'
+require 'tilt'
 
 # apparently wkhtmltopdf does try to support page-break-inside: avoid
 
@@ -13,43 +15,7 @@ stylesheets = %w[
   styles.css
 ]
 
-html = <<EOL
-<!doctype html>
-<html>
-<head>
-  <title>#{title}</title>
-  <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
-#{ stylesheets.map do |sheet|
-     "  <link rel='stylesheet' href='#{sheet}'/>"
-   end.join("\n") }
-</head>
-<body>
-
-<div class='block-group'>
-  <div class='metadata block'>
-    <div class='invoice'>Invoice 13</div>
-    <div class='date'>Sep 28, 2014</div>
-  </div>
-
-  <div class='sender block'>
-    <div class='name'>AUTHOR NAME<div>
-    <div class='address'>AUTHOR ADDRESS</div>
-    <div class='address2'>AUTHOR CITY, STATE ZIP</div>
-    <div class='phone'>AUTHOR PHONE</div>
-  </div>
-
-  <div class='recipient block'>
-    <div class='name'>CLIENT NAME</div>
-    <div class='address'>CLIENT ADDRESS</div>
-    <div class='address2'>CLIENT CITY, STATE ZIP</div>
-  </div>
-
-</div>
-
-</body>
-</html>
-EOL
-
+html = Tilt.new('invoice.slim').render(nil, title: title, stylesheets: stylesheets)
 File.write('inv.html', html)
 
 # can also pass params in meta tags: <head><meta name="pdfkit-page_size" content="Letter">...
