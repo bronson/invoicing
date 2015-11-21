8b0d269653c6716201e8d105bc6fa91f3c341b00
Scott Bronson <brons_git@rinspin.com>
2015-11-21 15:00:04 -0800

    minor cleanups to Range monkeypatches

--- range_fixes.rb
+++ range_fixes.rb
@@ -1,21 +1,22 @@
 # These are some monkeypatches that make the Range class a little nicer to use
 
 class Range
+  # returns the overlapping range, or an empty range if there's no overlap
   def intersection(other)
     return self.end..self.end if self.end < other.begin
     return other.end..other.end if other.end < self.begin
     [self.begin, other.begin].max..[self.end, other.end].min
   end
 
-  def empty?
-    # maybe it should be >= ?
-    self.begin == self.end
-  end
-
-  def span
-    self.end - self.begin    #   Range#size doesn't work
-  end
-
   alias_method :&, :intersection
+
+  def empty?
+    self.begin >= self.end
+  end
+
+  # Like Range#size but doesn't discriminate against non-Numerics (like Dates)
+  def span
+    self.end - self.begin
+  end
 end
 
